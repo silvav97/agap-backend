2a4b24a06f7c517d0fa88dd73694a52de959f46a
sebastian <sesilvavi@gmail.com>
2024-04-28 20:56:28 -0500

    Creo importCrop.sql

--- src/main/resources/importCropType.sql
+++ src/main/resources/importCropType.sql
@@ -1,3 +1,6 @@
 
 
-INSERT INTO crop_type (id, name, brand, price_per_gram) VALUES (1, 'pesticide1', 'pesticide1', 34.5);
+INSERT INTO crop_type (
+id, weather, name, plant_quantity_per_square_meter, harvest_time, fertilizer_quantity_per_plant, fertilizer_frequency, pesticide_quantity_per_plant, pesticide_frequency, fertilizer_id, pesticide_id)
+VALUES
+(1, 'weather', 'name', 1, 1, 1, 1, 1, 1, 1, 1);
