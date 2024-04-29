7f8760e92f0edc302d72d4d6c51900c9b4b3f542
ajvillav <ajvillav@unal.edu.co>
2024-04-28 20:11:55 -0500

    Eliminar campos innecesarios en DTOs

--- src/main/java/com/agap/management/domain/dtos/ExpenseDTO.java
+++ src/main/java/com/agap/management/domain/dtos/ExpenseDTO.java
@@ -2,7 +2,6 @@ package com.agap.management.domain.dtos;
 
 import com.agap.management.domain.enums.ExpenseType;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,10 +17,6 @@ import java.time.LocalDate;
 public class ExpenseDTO {
 
     private Integer id;
-
-    @NotNull(message = "Debes seleccionar un Cultivo ID.")
-    @Min(value = 1, message = "El valor debe ser mayor que cero")
-    private Integer cropId;
 
     private CropDTO crop;
 
