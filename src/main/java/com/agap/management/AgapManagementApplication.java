07a8864154e94e370d73cb390a5836a813925813
ajvillav <ajvillav@unal.edu.co>
2024-05-13 17:23:17 -0500

    Logica para que se envie notificaciones para insumos todos los dias a las 6 a.m

--- src/main/java/com/agap/management/AgapManagementApplication.java
+++ src/main/java/com/agap/management/AgapManagementApplication.java
@@ -10,11 +10,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableAsync
 public class AgapManagementApplication {
 
--- src/main/java/com/agap/management/application/ports/IEmailService.java
+++ src/main/java/com/agap/management/application/ports/IEmailService.java
@@ -3,6 +3,8 @@ package com.agap.management.application.ports;
 import jakarta.mail.MessagingException;
 import org.springframework.scheduling.annotation.Async;
 
+import java.util.Map;
+
 public interface IEmailService {
     @Async
     void sendEmail(String to, String subject, String content, String url, String buttonMessage) throws MessagingException;
